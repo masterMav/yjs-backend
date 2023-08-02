96355bac5c24403f704d68ac9434c4c7fa217444
masterMav <mavdoesdev@gmail.com>
2023-08-02 14:46:55 +0530

    2 docs parallely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,52 +6,65 @@ import { MonacoBinding } from "y-monaco";
 
 function App() {
   const editorRef = useRef(null);
+  const editorRef2 = useRef(null);
 
-  let binding = null, yarray = null, provider = null;
-  
+  let binding = null,
+    yarray = null,
+    provider = null;
+
+  // Initialize YJS
+  const ydoc = new Y.Doc();
+
+  // Create yArray
+  yarray = ydoc.getArray("monaco");
+
+  // Connect to websocket server
+  provider = new WebsocketProvider("ws://localhost:5000", "room1", ydoc);
+
+  // Fetch all data from server
+  provider.on("sync", () => {
+    if (yarray.length === 0) {
+      const newDoc = new Y.Text(),
+        doc2 = new Y.Text();
+      yarray.push([newDoc]);
+      yarray.push([doc2]);
+    }
+  });
+
+  // Bind doc1 -> editor1
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
-    
-    // Initialize YJS
-    const ydoc = new Y.Doc();
-  
-    // create yArray
-    yarray = ydoc.getArray("monaco");
-  
-    // connect to websocket server
-    provider = new WebsocketProvider("ws://localhost:5000", "room1", ydoc);
-
-    provider.on("sync", () => {
-      // Bind initial
-      if (yarray.length === 0) {
-        const newDoc = new Y.Text(),
-          doc2 = new Y.Text();
-        yarray.push([newDoc]);
-        yarray.push([doc2]);
-      }
-
-      // Bind YJS to Monaco
-      binding = new MonacoBinding(
-        yarray.get(0),
-        editorRef.current.getModel(),
-        new Set([editorRef.current]),
-        provider.awareness
-      );
-    });
-  }
-
-  // type.toString() shows proper text, for storage just pass type.toString()
-  const handleClick = () => {
-
-    // editor history gets deleted
-    binding.destroy();
 
     binding = new MonacoBinding(
-      yarray.get(1),
+      yarray.get(0),
       editorRef.current.getModel(),
       new Set([editorRef.current]),
       provider.awareness
     );
+  }
+
+  // Bind doc2 -> editor2
+  function handleEditorDidMount2(editor, monaco) {
+    editorRef2.current = editor;
+
+    binding = new MonacoBinding(
+      yarray.get(1),
+      editorRef2.current.getModel(),
+      new Set([editorRef2.current]),
+      provider.awareness
+    );
+  }
+
+  // type.toString() shows proper text, for storage just pass type.toString()
+  const handleClick = () => {
+    // editor history gets deleted
+    // binding.destroy();
+    // binding = new MonacoBinding(
+    //   yarray.get(1),
+    //   editorRef.current.getModel(),
+    //   new Set([editorRef.current]),
+    //   provider.awareness
+    // );
   };
 
   return (
@@ -60,9 +73,15 @@ function App() {
       <button onClick={handleClick}>Switch</button>
       <Editor
         height="50vh"
-        width="50vw"
+        width="100vw"
         theme="vs-dark"
         onMount={handleEditorDidMount}
+      />
+      <Editor
+        height="50vh"
+        width="100vw"
+        theme="vs-dark"
+        onMount={handleEditorDidMount2}
       />
     </>
   );
